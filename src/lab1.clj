e97a61f77591d6a0d4dd053b2d4f571771226fa5
STMND <72205334+STMND@users.noreply.github.com>
2023-10-19 12:03:50 +0300

    Update lab1.clj

--- src/lab1.clj
+++ src/lab1.clj
@@ -1,4 +1,21 @@
 
-(def rand_seq (repeatedly #(rand-int 50)))
-(print (take (+ (rand-int 5) 5) rand_seq))
+(defn find_min_in_seq []
+  (def rand_seq (repeatedly #(rand-int 50)))
+  (println (take (+ (rand-int 10) 8) rand_seq))
+  (def v (take (+ (rand-int 10) 8) rand_seq))
+  (loop [shrinking (rest v)
+         this-idx 1
+         min-val (first v)
+         min-idx 0]
+    (if (empty? shrinking)
+      {:val min-val :idx min-idx}
+      (let [this-val (first shrinking)
+            new-min? (< this-val min-val)]
+        (recur (rest shrinking)
+               (inc this-idx)
+               (if new-min? this-val min-val)
+               (if new-min? this-idx min-idx)
+               )))))
+
+(println(find_min_in_seq))
 
